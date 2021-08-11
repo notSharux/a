3d8d6e9444b388c24109e88a39447534cc15f67b
notSharux <85288797+notSharux@users.noreply.github.com>
2021-08-11 13:34:09 +0200

    Add files via upload

---
+++ sus.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+sudo pacman -S xorg xorg-server xorg-xinit
+
+git clone https://github.com/notSharux/dotfiles
+git clone https://github.com/notSharux/st-xrdb
+git clone https://github.com/notSharux/dmenu-A
+git clone https://github.com/notSharux/dwm-xrdb
+git clone https://aur.archlinux.org/yay
+
+rm -rfv .config
+
+cd dotfiles
+cp .xinitrc ~
+cp -r .config ~
+cp .Xresources ~
+cd
+
+cd st-xrdb
+sudo make clean install
+cd
+
+#cd dmenu-A
+#sudo make clean install
+#cd
+
+cd dwm-xrdb
+sudo make clean install
+cd
+
+cd yay
+makepkg -si
