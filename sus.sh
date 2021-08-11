7982222d88bef610c1d6a679fc60b9be36fb1b29
notSharux <85288797+notSharux@users.noreply.github.com>
2021-08-11 15:00:09 +0200

    Update sus.sh

--- sus.sh
+++ sus.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-sudo pacman -S xorg xorg-server xorg-xinit base-devel ttf-jetbrains-mono
+sudo pacman -S xorg xorg-server xorg-xinit ttf-jetbrains-mono
 
+cd ~
 git clone https://github.com/notSharux/dotfiles
 git clone https://github.com/notSharux/st-xrdb
-git clone https://github.com/notSharux/dmenu-A
+git clone https://github.com/notSharux/dmenu
 git clone https://github.com/notSharux/dwm-xrdb
 git clone https://aur.archlinux.org/yay
+
+cd dotfiles/random
+./time.sh
+cd
 
 rm -rfv .config
 
@@ -14,17 +19,22 @@ cd dotfiles
 cp .xinitrc ~
 cp -r .config ~
 cp .Xresources ~
+cp -r dwmblocks ~
 cd
 
 cd st-xrdb
 sudo make clean install
 cd
 
-#cd dmenu-A
-#sudo make clean install
-#cd
+cd dmenu
+sudo make clean install
+cd
 
 cd dwm-xrdb
+sudo make clean install
+cd
+
+cd dwmblocks
 sudo make clean install
 cd
 
