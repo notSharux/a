408c9d2ece99de955e8ad09067fbc36e2cf22f3b
notSharux <85288797+notSharux@users.noreply.github.com>
2021-09-12 16:31:23 +0200

    Update sus.sh

--- sus.sh
+++ sus.sh
@@ -3,22 +3,22 @@
 #!/bin/bash
 
 # Update
-yes | sudo pacman -Syyu
+sudo pacman -Syyu --noconfirm
 
 # Xorg
-sudo pacman -S xorg-xrandr xorg-server xorg-xinit libxft libxinerama
+sudo pacman -S --noconfirm xorg-xrandr xorg-server xorg-xinit libxft libxinerama
 
 # Font
-sudo pacman -S ttf-jetbrains-mono ttf-font-awesome ttf-joypixels gnu-free-fonts noto-fonts ttf-hack powerline-fonts
+sudo pacman -S --noconfirm ttf-jetbrains-mono ttf-font-awesome # ttf-joypixels gnu-free-fonts noto-fonts ttf-hack powerline-fonts
 
 # Drivers
 sudo pacman -S nvidia nvidia-settings
 
 # Audio
-sudo pacman -S pulseaudio pavucontrol
+sudo pacman -S --noconfirm pulseaudio pavucontrol
 
 # Other
-sudo pacman -S zsh-syntax-highlighting zsh-autosuggestions zsh zsh-completions starship xplr neovim base-devel alacritty  fuse ntfs-3g firefox nodejs nitrogen fzf ripgrep the_silver_searcher fd gvfs
+sudo pacman -S --noconfirm zsh-syntax-highlighting zsh-autosuggestions zsh zsh-completions starship neovim base-devel alacritty  fuse ntfs-3g firefox nitrogen fzf ripgrep the_silver_searcher fd gvfs lxsession lxappearance
 
 cd ~
 # dotfiles
@@ -31,6 +31,7 @@ git clone https://github.com/notSharux/st-xrdb
 git clone https://github.com/notSharux/dmenu-xrdb
 git clone https://github.com/notSharux/dwm-xrdb
 git clone https://github.com/notSharux/slstatus
+git clone https://github.com/dylanaraps/pfetch
 cd
 
 # AUR helper
@@ -52,6 +53,7 @@ cp -r wallpapers ~
 cp .bashrc ~
 cp .Xresources ~
 cp .zshrc ~
+cp .zshenv ~
 cp -r dwmblocks ~/Suckless-Builds/
 cd
 
@@ -67,6 +69,10 @@ cd
 # Suckless builds
 cd Suckless-Builds/.config/dwmblocks
 sudo chmod +x *
+cd
+
+cd Suckless-Builds/pfetch
+sudo make install
 cd
 
 cd Suckless-Builds/st-xrdb
@@ -116,15 +122,15 @@ sudo chmod +x time.sh
 cd
 
 # Fonts
-git clone https://github.com/ryanoasis/nerd-fonts
-cd nerd-fonts
-sudo chmod +x install.sh
-cd
+#git clone https://github.com/ryanoasis/nerd-fonts
+#cd nerd-fonts
+#sudo chmod +x install.sh
+#cd
 
-git clone https://github.com/powerline/ 
-cd fonts
-sudo chmod +x install.sh
-cd
+#git clone https://github.com/powerline/
+#cd fonts
+#sudo chmod +x install.sh
+#cd
 
-rm -rfv a ChromeOS-theme dotfiles yay Tela-icon-theme picom nerd-fonts fonts
+rm -rfv a ChromeOS-theme dotfiles yay Tela-icon-theme picom # nerd-fonts fonts
 
